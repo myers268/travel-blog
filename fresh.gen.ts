334742fd06b92031496f3ccf8c33a17f7262f211
Zack Myers <42181342+myers268@users.noreply.github.com>
2022-08-18 11:26:01 -1000

    Scaffolded basic routes
    Added css and fonts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,16 +2,18 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
+import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/trips/[trip].tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
+    "./routes/_app.tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
+    "./routes/trips/[trip].tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/_app.tsx
@@ -0,0 +1,21 @@
+/** @jsx h */
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
+import { Head } from"$fresh/runtime.ts";
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/styles/global.css" />
+        <title>Traveling With</title>
+        <meta
+          name="description"
+          content="Traveling around the world with your's truly."
+        />
+      </Head>
+      <Component />
+    </>
+  )
+}
