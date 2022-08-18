7cecb00fdf82d5d497abc5ceeb35a18d00a25979
Zack Myers <42181342+myers268@users.noreply.github.com>
2022-08-18 12:38:45 -1000

    Added basic article layout

--- routes/trips/[trip].tsx
+++ routes/trips/[trip].tsx
@@ -3,5 +3,24 @@ import { h } from "preact";
 import { PageProps } from "$fresh/server.ts";
 
 export default function Greet({ params }: PageProps) {
-  return <div>Trip to {params.trip}</div>;
+  return (
+    <div>
+      <h1>Trip to {params.trip}</h1>
+      <section>
+        <aside>Mermaid caves</aside>
+        <aside>
+          <img src="/mermaid-caves-1.jpeg" />
+        </aside>
+      </section>
+      <section>
+        Blah blah blah
+      </section>
+      <section>
+        <aside>Mermaid caves</aside>
+        <aside>
+          <img src="/mermaid-caves-2.jpeg" />
+        </aside>
+      </section>
+    </div>
+  );
 }
